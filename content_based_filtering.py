095c170c176bf63163be4b98e170dd8ebbb08087
Vishal <vishal.u338@gmail.com>
2022-09-29 20:06:22 +0530

    Add files via upload

---
+++ content_based_filtering.py
@@ -0,0 +1,25 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("final.csv")
+
+df = df[df["soup"].notna()]
+
+count = CountVectorizer(stop_words="English")
+count_matrix = count.fit_transform(df["soup"])
+
+cosine_sim = cosine_similarity(count_matrix,count)
+df = df.reset_index()
+indices = pd.Series(df.index,index=df["title"])
+
+def get_recommendations(title):
+    idx = indices[title]
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    sim_scores = sorted(sim_scores, key = lambda x: x[1], reverse = True)
+    sim_scores = sim_scores[1:11]
+    movie_indices = [i[0] for i in sim_scores]
+    return df[['title', 'poster_link', 'release_date', 'runtime', 'vote_count', 'vote_average', 'overview']].iloc[movie_indices].values.tolist()
+
+
---
+++ merge_csv.py
@@ -0,0 +1,31 @@
+import csv
+
+# all_movies = []
+
+with open("movies.csv","r",encoding="UTF-8") as f:
+    reader = csv.reader(f)
+    data = list(reader)
+    all_movies = data[1:]
+    headers = data[0]
+
+headers.append("poster_link")
+
+with open("final.csv","a+",encoding="UTF-8") as f:
+    csv_writer = csv.writer(f)
+    csv_writer.writerow(headers)
+
+with open("movie_links.csv","r",encoding="UTF-8") as f:
+    reader = csv.reader(f)
+    data = list(reader)
+    all_movie_links = data[1:]
+
+for movie_item in all_movies:
+    poster_found = any(movie_item[8] in movie_link_item for movie_link_item in all_movie_links)
+    if poster_found:
+        for movie_link_item in all_movie_links:
+            if movie_item[8] == movie_link_item[0]:
+                movie_item.append(movie_link_item[1])
+                if len(movie_item) == 28:
+                    with open("final.csv","a+",encoding="UTF-8") as f:
+                        csv_writer = csv.writer(f)
+                        csv_writer.writerow(movie_item)
